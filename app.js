de6cb42b60894c1f93341deea6fae1cbd9104d12
Gaurav Singh <gauravkarki038@gmail.com>
2023-06-21 13:47:12 +0530

    issue resolve

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require("path");
 
 app.use(cors({
   origin: 'https://eshop-market-frontend.vercel.app',
+  // origin: 'http://localhost:3000',
   credentials: true
 }));
 
@@ -31,13 +32,11 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 const user = require("./controller/user");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
-const event = require("./controller/event");
 const coupon = require("./controller/coupounCode");
 const payment = require("./controller/payment");
 const order = require("./controller/order");
 const conversation = require("./controller/conversation");
 const message = require("./controller/message");
-const withdraw = require("./controller/withdraw");
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/conversation", conversation);
@@ -45,10 +44,8 @@ app.use("/api/v2/message", message);
 app.use("/api/v2/order", order);
 app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
-app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
 app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
